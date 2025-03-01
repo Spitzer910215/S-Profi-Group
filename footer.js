38969fd21e389da01268683dd44c69fb02253d30
Spitzer910215 <e.kim@sprofigroup.com>
2025-03-01 22:06:44 +0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     fetch("footer.html")
         .then(response => response.text())
         .then(data => {
-            document.getElementById("footer-container").innerHTML = data;
-        });
+            document.getElementById("footer").innerHTML = data;
+        })
+        .catch(error => console.error("Ошибка загрузки footer.html:", error));
 });
